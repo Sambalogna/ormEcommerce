84cad2e916430b3b2aede8baafb873d60dd06c2b
Samuel Mandel <slman.760@gmail.com>
2022-05-05 19:08:31 -0700

    add delete route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,9 +106,14 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
-
-  } catch {
-
+    const productData = await Product.destroy({
+      where:{
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err)
   }
 });
 
